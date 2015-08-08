7b449bf0cd47b7dc262aa46fae439e35e7ba67d7
Prashant Chaturvedi <pc.iiitmk@gmail.com>
2015-08-08 20:37:06 +0530

    Initial commit

---
+++ src/labs/pc/ce/medium/reverse_and_add/Main.java
@@ -0,0 +1,58 @@
+package labs.pc.ce.medium.reverse_and_add;
+
+import java.io.*;
+
+/**
+ * Reverse and add.
+ * @author Prashant Chaturvedi
+ *
+ */
+public class Main {
+	private static final String INPUT_FILE_NAME = "input_file";
+	
+	public static void main (String[] args) {
+		/*
+		 * If the application is running locally, use this input file name. On the server,
+		 * the server passes the name of the file as a command line argument when executing
+		 * the code.
+		 */
+		File file = new File (args.length != 1? INPUT_FILE_NAME : args[0]);
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader (new FileReader(file));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				int number = Integer.parseInt(line);
+				int count = 0;
+				while (!isPalendrome(number)) {
+					count++;
+					number = number + reverse(number);
+				}
+				System.out.println(count + " " + number);
+			}
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException ioe) {
+					ioe.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public static int reverse (int number) {
+		int sum = 0;
+		while (number != 0) {
+			sum = (sum * 10) + (number % 10);
+			number = number / 10;
+		}
+		return sum;
+	}
+	
+	public static boolean isPalendrome (int number) {
+		return number == reverse (number);
+	}
+}
